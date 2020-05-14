d2b17cbf111daee1653bac4f9e45e2373112512f
Allan Brown <albrown111@hotmail.com>
2020-05-14 11:31:30 -0700

    Week 2 Assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { DishService } from './services/dish.service';
 import { PromotionService} from './services/promotion.service';
+import { LeaderService} from './services/leader.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
