7065ceabb0c983e5e6b8096fcf1ed3c060492544
Allan Brown <albrown111@hotmail.com>
2020-06-08 15:01:11 -0700

    Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -11,7 +11,20 @@ import { Comment } from '../shared/comment';
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+        state('shown', style({
+            transform: 'scale(1.0)',
+            opacity: 1
+        })),
+        state('hidden', style({
+            transform: 'scale(0.5)',
+            opacity: 0
+        })),
+        transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 
 
@@ -31,6 +44,8 @@ export class DishdetailComponent implements OnInit {
   next: string;
   errMess: string;
 
+  visibility = 'shown';
+
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
     private location: Location,
@@ -40,10 +55,20 @@ export class DishdetailComponent implements OnInit {
     }
 
   ngOnInit() {
+
+
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params
-      .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
+      .pipe(switchMap((params: Params) => {
+        this.visibility='hidden';
+        return this.dishservice.getDish(params['id']);
+      }))
+      .subscribe(dish => {
+        this.dish = dish;
+        this.dishcopy = dish;
+        this.setPrevNext(dish.id);
+        this.visibility = 'shown';
+      },
       errMess => this.errMess = <any>errMess);
   }
 
